97bf61e2c6daa338aac7a35f2181d1a1dd0eb67b
Deepesh Belani <60230543+QGDeep@users.noreply.github.com>
2026-01-23 16:05:33 +0400

    Delete Source/ParkourSystem/MoveableObject.cpp

--- Source/ParkourSystem/MoveableObject.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "MoveableObject.h"
-
-// Add default functionality here for any IMoveableObject functions that are not pure virtual.
